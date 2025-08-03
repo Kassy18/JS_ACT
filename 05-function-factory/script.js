7bbb2dc630a83db091e8bde6fe8cc533603888f0
Kassy18 <kassandraandal601@gmail.com>
2025-08-03 14:14:40 +0800

    Update script.js

--- 05-function-factory/script.js
+++ 05-function-factory/script.js
@@ -1,1 +1,18 @@
+const double = makeMultiplier(2);
+const sayHi = makeGreeter("Hi");
+    
+function makeMultiplier(factor) {
+   return function (number) {
+       return number * factor;
+    };
+}
 
+function makeGreeter(greeting) {
+   return function (name) {
+        return `${greeting}, ${name}!`;
+   };
+}
+
+document.getElementById("doubleNumber").innerText = "Double of number 5: " + double(5);      
+document.getElementById("greet").innerText = 'Greetings: ' + sayHi("Mia"); 
+ 
